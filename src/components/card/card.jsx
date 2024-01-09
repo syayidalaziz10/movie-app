7da4de98a1fb1c39b9df47ad4c3e6458c5eacf0a
syayidalaziz10 <shorashiro15@gmail.com>
2024-01-09 19:47:32 +0700

    configure key image

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 
 
 export default function Card ({movie}) {
-   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
-
    return (
       <div className="scale-90 lg:scale-100">
          <Image
@@ -12,6 +10,7 @@ export default function Card ({movie}) {
             width = {500}
             height = {500}
             className="w-44 h-44 mb-3 rounded-lg"
+            key = {movie.id}
          />
          <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">{movie.original_title}</div>
       </div>
