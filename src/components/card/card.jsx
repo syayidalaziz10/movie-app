5d8bce93592f8b3e7251686c8027eb9d913905b5
syayidalaziz10 <shorashiro15@gmail.com>
2024-01-09 19:54:07 +0700

    fix key id in movie card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,14 +3,13 @@ import Image from 'next/image'
 
 export default function Card ({movie}) {
    return (
-      <div className="scale-90 lg:scale-100">
+      <div className="scale-90 lg:scale-100" key = {movie.id}>
          <Image
             src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
             alt="image poster"
             width = {500}
             height = {500}
             className="w-44 h-44 mb-3 rounded-lg"
-            key = {movie.id}
          />
          <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">{movie.original_title}</div>
       </div>
