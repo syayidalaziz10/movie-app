3ad79b6f5d8f51adaac5513e511b6c57a8297b12
syayidalaziz10 <shorashiro15@gmail.com>
2024-01-09 10:53:16 +0700

    add responsive page in other device

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,20 +4,20 @@ import { SunIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
    return (
-      <header className="flex z-50 justify-center sticky top-0 w-full h-fit border-b border-opacity-40 bg-white bg-opacity-50 backdrop-blur-[0.5rem]">
-         <nav className="w-1/2 flex justify-between items-center p-4">
+      <header className="flex z-50 justify-center sticky top-0 w-full h-fit border-b border-opacity-40 bg-white">
+         <nav className="w-full lg:w-1/2 flex justify-between items-center p-4">
             <div className='flex items-center gap-3'>
                {/* <VideoCameraIcon className='w-9' /> */}
-               <h1 className="uppercase font-bold text-xl">Net movie</h1>
+               <h1 className="uppercase font-bold md:text-xl">Net movie</h1>
             </div>
-            <div className='flex items-center gap-10'>
+            <div className='flex items-center gap-3'>
                <div className='relative'>
                   <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
                      <MagnifyingGlassIcon className='w-5 text-gray-400' />
                   </div>
                   <input
                      type="text"
-                     className="block ps-10 py-5 px-2 h-8 outline-none bg-gray-100 text-gray-400 text-sm rounded-md"
+                     className="block ps-10 py-5 px-2 h-8 outline-none bg-gray-100 text-gray-400 md:text-sm text-xs rounded-md"
                      placeholder="Search movies ... "
                   />
                </div>
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,9 +2,10 @@ import Image from "next/image"
 import { StarIcon } from '@heroicons/react/24/solid'
 import BannerImage from './banner.jpg'
 
+
 export default function Main() {
    return (
-      <main className="flex flex-col justify-center items-center p-2 my-10 w-1/2 h-fit gap-10">
+      <main className="flex flex-col justify-center items-center p-2 my-10 lg:w-1/2 h-fit gap-10">
          <div className="w-full relative">
             <Image
                src={BannerImage}
@@ -13,16 +14,16 @@ export default function Main() {
             />
             <div className='absolute inset-y-0 start-0 flex items-center p-10 pointer-events-none'>
                <div className="flex justify-start flex-col gap-2 text-left">
-                  <h3 className="text-white text-base">Action, Comedy</h3>
-                  <h1 className="text-3xl text-white">The Hunger Games: The Ballad of Songbirds & Snakes</h1>
-                  <p className="text-white/[0.6]">64 years before he becomes the tyrannical president of Panem, Coriolanus Snow sees a chance for a change in fortunes when he mentors Lucy Gray Baird, the female tribute from District 12. </p>
+                  <h3 className="text-white md:text-base text-xs">Action, Comedy</h3>
+                  <h1 className="md:text-3xl text-lg text-white">The Hunger Games: The Ballad of Songbirds & Snakes</h1>
+                  <p className="text-white/[0.6] text-xs md:text-base">64 years before he becomes the tyrannical president of Panem, Coriolanus Snow sees a chance for a change in fortunes when he mentors Lucy Gray Baird, the female tribute from District 12. </p>
                   {/* <button className="px-4 py-1 text-center text-white text-sm bg-white bg-opacity-40 backdrop-blur-sm w-fit h-10 rounded-full">Visit here</button> */}
                </div>
             </div>
          </div>
          <div className="w-full h-fit">
             <div className="flex items-center justify-between">
-               <h1 className="text-left font-semibold text-3xl text-black">Discover Movies</h1>
+               <h1 className="text-left font-semibold md:text-3xl text-xl text-black">Discover Movies</h1>
                <form action="">
                   <select name="" id="" className="px-2 py-3 bg-gray-100 rounded-md text-sm outline-none">
                      <option value="">All</option>
@@ -32,15 +33,60 @@ export default function Main() {
                </form>
             </div>
          </div>
-         <div className="w-full flex flex-wrap justify-start items-center gap-3">
-            <div className="flex flex-col items-center gap-3">
+         <div className="w-full flex flex-wrap justify-start items-center gap-y-2 gap-x-3">
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
                <Image
                   src={BannerImage}
                   object-fit="cover"
                   alt="Fav movie banner"
                   className="w-44 h-44 rounded-lg"
                />
-               <div className="w-44 h-fit text-center text-base tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+            </div>
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
+               <Image
+                  src={BannerImage}
+                  object-fit="cover"
+                  alt="Fav movie banner"
+                  className="w-44 h-44 rounded-lg"
+               />
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+            </div>
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
+               <Image
+                  src={BannerImage}
+                  object-fit="cover"
+                  alt="Fav movie banner"
+                  className="w-44 h-44 rounded-lg"
+               />
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+            </div>
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
+               <Image
+                  src={BannerImage}
+                  object-fit="cover"
+                  alt="Fav movie banner"
+                  className="w-44 h-44 rounded-lg"
+               />
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+            </div>
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
+               <Image
+                  src={BannerImage}
+                  object-fit="cover"
+                  alt="Fav movie banner"
+                  className="w-44 h-44 rounded-lg"
+               />
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
+            </div>
+            <div className="flex flex-col items-center gap-3 scale-95 lg:scale-100">
+               <Image
+                  src={BannerImage}
+                  object-fit="cover"
+                  alt="Fav movie banner"
+                  className="w-44 h-44 rounded-lg"
+               />
+               <div className="w-44 h-fit text-center md:text-base text-sm tracking-tight leading-normal text-clip font-semibold text-black/[0.8]">Hunger Games</div>
             </div>
          </div>
       </main>
